8632cf3ccb3b1682caf58cede1f277383db0f946
badrak <andrewbadrak29@gmail.com>
2018-03-24 13:37:49 +0200

    Add files via upload

--- modul/script/js.js
+++ modul/script/js.js
@@ -1,1 +1,13 @@
+$(".square").mouseenter(function(){
+	$(".rectangle").css("visibility", "visible");
+});
 
+
+
+$("#triangle-down").mouseenter(function(){
+	$(".rectangle").animate({bottom:'0',left:'0'}, 3000);
+});
+
+$("#triangle").mouseenter(function(){
+	$(".rectangle").animate({bottom:'0',left:'0'}, 3000);
+});
