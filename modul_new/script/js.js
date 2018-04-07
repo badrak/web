ee4e8195aaf03e9b31ab667e2f6d4644057420ad
badrak <andrewbadrak29@gmail.com>
2018-04-07 12:04:14 +0300

    Add files via upload

--- modul_new/script/js.js
+++ modul_new/script/js.js
@@ -1,1 +1,16 @@
+$(".square").mouseenter(function(){
+	$(".mini_square").css("visibility", "visible");
+});
 
+$("#triangle").mouseenter(function(){
+	$(".mini_square_2").css("visibility", "visible");
+});
+
+
+$("#triangle").mouseleave(function(){
+	$(".mini_square").animate({left:'0'}, 3000);
+});
+
+$("#triangle").mouseleave(function(){
+	$(".mini_square_2").animate({bottom:'0'}, 3000);
+});
